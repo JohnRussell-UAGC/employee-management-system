21d8897bc8773e21b553d93dec7ca28051bfb517
Int-304 <John.Russell@faculty.uagc.edu>
2023-06-29 13:03:13 -0600

    Assignment-3
    Update the App.js to use local storage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import EmployeeForm from './Components/EmployeeForm';
 
 function App() {
+  // define the property for the employee, the function, and the default state.
+  const [employees, setEmployees] = useState([]);
+
+  // maintain an array of current employees
+  const addEmployee = (employee) => {
+    setEmployees([...employees, employee]);
+  };
+
+  // save the employee array to local storage
+  const saveData = () => {
+    localStorage.setItem('employees', JSON.stringify(employees));
+  };
+
   return (
-    <EmployeeForm></EmployeeForm>
+    // add the div because JSX requires a singular parent, and we are adding a button
+    <div>
+      <EmployeeForm onSubmit={addEmployee} /> 
+      <button onClick={saveData}>Save Data</button>
+    </div>
   );
 }
 
